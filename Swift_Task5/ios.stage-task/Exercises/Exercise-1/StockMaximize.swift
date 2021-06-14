f64b802557cca1df7f0f5eb1c664b999ae10640f
Andrey Egorov <andreyegorov1985@gmail.com>
2021-06-14 15:40:06 +0300

    feat: add exercise 1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,34 @@
 import Foundation
 
 class StockMaximize {
-
+    var shares = 0
+    var spent = 0
+    var profit = 0
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count < 2 {
+            return 0
+        }
+        
+        var next: Array<Int>
+        var maxPrice = prices.max()
+        
+        for (i, price) in prices.enumerated() {
+            if price == maxPrice {
+                self.profit += (self.shares * price) - self.spent
+                self.shares = 0
+                self.spent = 0
+                if i + 1 < prices.count - 1 {
+                    next = Array(prices[i + 1...prices.count - 1])
+                } else {
+                    break
+                }
+                maxPrice = next.max()
+                continue
+            }
+            self.shares += 1
+            self.spent += price
+        }
+        return self.profit
     }
 }
