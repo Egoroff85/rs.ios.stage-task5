291495f0f9294c1e3a7a5a38a0afb13cfb2e7bdb
Andrey Egorov <andreyegorov1985@gmail.com>
2021-06-14 21:58:17 +0300

    feat: add exercises 2,3

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,36 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
+    var stack: [(Int, String)] = []
+    var result: String = ""
+    var num: Int = 0
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var encryptedStr = message
+        
+        if encryptedStr.hasPrefix("[") && encryptedStr.hasSuffix("]") {
+            encryptedStr.removeFirst()
+            encryptedStr.removeLast()
+        }
+        
+        for char in Array(encryptedStr) {
+            if char.isLetter {
+                self.result += String(char)
+            } else if char.isNumber {
+                self.num = self.num * 10 + Int(String(char))!
+            } else if char == "[" {
+                self.stack.append((self.num, self.result))
+                self.num = 0
+                self.result = ""
+            } else if char == "]" {
+                let temp = self.stack.popLast()!
+                var repeatStr = ""
+                for _ in 0..<temp.0 {
+                    repeatStr += self.result
+                }
+                self.result = temp.1 + repeatStr
+            }
+        }
+        return self.result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -16,7 +16,43 @@ public final class Knapsack {
         self.foods = foods
     }
     
+    func knapSack(supplies: [Supply]) -> [[Int]] {
+        let n = supplies.count
+
+        var t = Array(repeating: Array(repeating: 0, count: self.maxWeight + 1), count: n + 1)
+        for i in 0..<n + 1 {
+            for j in 0..<self.maxWeight + 1 {
+                if i == 0 || j == 0 {
+                    t[i][j] = 0
+                } else if supplies[i - 1].weight <= j {
+                    t[i][j] = max(supplies[i - 1].value + t[i - 1][j - supplies[i - 1].weight], t[i - 1][j])
+                } else {
+                    t[i][j] = t[i - 1][j]
+                }
+            }
+        }        
+        return t
+    }
+    
     func findMaxKilometres() -> Int {
-        return -1
+        var maxDistance = 0
+        
+        if self.maxWeight < 0 {
+            return maxDistance
+        }
+
+        let foodsMatrix = knapSack(supplies: self.foods)
+        let drinksMatrix = knapSack(supplies: self.drinks)
+        
+        let foodsCount = self.foods.count
+        let drinksCount = self.drinks.count
+        
+        for weight in 0...self.maxWeight {
+            let currentMaxDistance = min(foodsMatrix[foodsCount][weight], drinksMatrix[drinksCount][self.maxWeight - weight])
+            if currentMaxDistance > maxDistance {
+                maxDistance = currentMaxDistance
+            }
+        }
+        return maxDistance
     }
 }
